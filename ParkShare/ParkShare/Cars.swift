e6e9188da1cf95cbe0757fd3ea009630f72815f7
AleFacc <117638646+AleFacc@users.noreply.github.com>
2023-10-05 15:55:07 +0200

    Schermate MyCars e Add new Car

--- ParkShare/ParkShare/Cars.swift
+++ ParkShare/ParkShare/Cars.swift
@@ -8,10 +8,79 @@
 import SwiftUI
 
 struct Cars: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack{
+                Text("My Cars")
+                    .font(
+                        .system(size: 40).bold()
+                    )
+                    .foregroundColor(Color(red: 0.38, green: 0.24, blue: 0.92))
+                
+                HStack{
+                    Spacer()
+                    NavigationLink(destination: AddCar()){
+                        Image(systemName: "plus")
+                        Text("Add new Car")
+                    }
+                }.padding(30)
+                
+                Rectangle()
+                .foregroundColor(.clear)
+                .frame(width: 375, height: 132)
+                .background(Color(red: 0.4, green: 0.42, blue: 0.93))
+                .cornerRadius(30)
+                .shadow(color: Color(red: 0.4, green: 0.42, blue: 0.93).opacity(0.2), radius: 15, x: 0, y: 2)
+                .overlay(
+                    VStack(spacing: 20){
+                        
+                        Text("Dati auto")   //Creare una struct per poterla utilizzare N volte, tramite forEach in lettura da Firebase
+                        
+                        
+                    }.foregroundColor(.white)
+                )
+                
+                Spacer()
+                
+            }
+        }
+    }
+    
+}
+
+
+
+struct AddCar: View {
+    @State private var modello: String = ""
+    @State private var targa: String = ""
+
+    var body: some View {
+        Form{
+            VStack{
+                HStack{
+                    
+                    TextField("", text: $modello, prompt: Text("Modello").foregroundColor(Color.red))
+                            .padding()
+                }
+                HStack{
+                    
+                    TextField("", text: $targa, prompt: Text("Targa").foregroundColor(Color.red))
+                            .padding()
+                }
+                Button(action: SalvaCar){
+                    Text("Salva")
+                }.disabled(modello.isEmpty || targa.isEmpty)
+                    
+                }
+        }
+    }
+    
+    func SalvaCar() {
+        //salvataggio dati su Firebase
     }
 }
+
 
 struct Cars_Previews: PreviewProvider {
     static var previews: some View {
